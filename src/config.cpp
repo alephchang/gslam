2dbd2efb88f97b9a051f50af8488f677573008d1
alephchang <alephchang@foxmail.com>
2017-12-29 11:12:43 +0800

    Initial commit

---
+++ src/config.cpp
@@ -0,0 +1,66 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2016  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "gslam/config.h"
+
+namespace gslam 
+{
+    
+void Config::setParameterFile( const std::string& filename )
+{
+    if ( config_ == nullptr )
+        config_ = shared_ptr<Config>(new Config);
+    config_->file_ = cv::FileStorage( filename.c_str(), cv::FileStorage::READ );
+    if ( config_->file_.isOpened() == false )
+    {
+        std::cerr<<"parameter file "<<filename<<" does not exist."<<std::endl;
+        config_->file_.release();
+        return;
+    }
+}
+
+Config::~Config()
+{
+    if ( file_.isOpened() )
+        file_.release();
+}
+
+shared_ptr<Config> Config::config_ = nullptr;
+
+}
+/*
+g2o_core_d.lib
+g2o_stuff_d.lib
+g2o_solver_csparse_d.lib
+g2o_types_slam3d_d.lib
+g2o_opengl_helper_d.lib
+g2o_csparse_extension_d.lib
+g2o_ext_csparse_d.lib
+g2o_types_icp_d.lib
+g2o_types_sba_d.lib
+opencv_core330d.lib
+opencv_highgui330d.lib
+opencv_imgproc330d.lib
+opencv_imgcodecs330d.lib
+opencv_calib3d330d.lib
+opencv_features2d330d.lib
+opencv_xfeatures2d330d.lib
+opencv_flann330d.lib
+opencv_vi330d.lib
+*/
---
+++ test/run_vo.cpp
@@ -0,0 +1,112 @@
+// -------------- test the visual odometry -------------
+#include <fstream>
+//#include <boost/timer.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/viz.hpp> 
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include "gslam/config.h"
+#include "gslam/visual_odometry.h"
+
+int main ( int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        cout<<"usage: run_vo parameter_file"<<endl;
+        return 1;
+    }
+
+    gslam::Config::setParameterFile ( argv[1] );
+    gslam::VisualOdometry::Ptr vo ( new gslam::VisualOdometry );
+
+    string dataset_dir = gslam::Config::get<string> ( "dataset_dir" );
+    cout<<"dataset: "<<dataset_dir<<endl;
+    ifstream fin ( dataset_dir+"/associate.txt" );
+    if ( !fin )
+    {
+        cout<<"please generate the associate file called associate.txt!"<<endl;
+        return 1;
+    }
+
+    vector<string> rgb_files, depth_files;
+    vector<double> rgb_times, depth_times;
+    while ( !fin.eof() )
+    {
+        string rgb_time, rgb_file, depth_time, depth_file;
+        fin>>rgb_time>>rgb_file>>depth_time>>depth_file;
+        rgb_times.push_back ( atof ( rgb_time.c_str() ) );
+        depth_times.push_back ( atof ( depth_time.c_str() ) );
+        rgb_files.push_back ( dataset_dir+"/"+rgb_file );
+        depth_files.push_back ( dataset_dir+"/"+depth_file );
+
+        if ( fin.good() == false )
+            break;
+    }
+
+    gslam::Camera::Ptr camera ( new gslam::Camera );
+
+    // visualization
+    cv::viz::Viz3d vis ( "Visual Odometry" );
+    cv::viz::WCoordinateSystem world_coor ( 1.0 ), camera_coor ( 0.5 );
+    cv::Point3d cam_pos ( 0, -1.0, -1.0 ), cam_focal_point ( 0,0,0 ), cam_y_dir ( 0,1,0 );
+    cv::Affine3d cam_pose = cv::viz::makeCameraPose ( cam_pos, cam_focal_point, cam_y_dir );
+    vis.setViewerPose ( cam_pose );
+
+    world_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 2.0 );
+    camera_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 1.0 );
+    vis.showWidget ( "World", world_coor );
+    vis.showWidget ( "Camera", camera_coor );
+
+    cout<<"read total "<<rgb_files.size() <<" entries"<<endl;
+    for ( int i=0; i<rgb_files.size(); i++ )
+    {
+        cout<<"****** loop "<<i<<" ******"<<endl;
+        Mat color = cv::imread ( rgb_files[i] );
+        Mat depth = cv::imread ( depth_files[i], -1 );
+        if ( color.data==nullptr || depth.data==nullptr )
+            break;
+        gslam::Frame::Ptr pFrame = gslam::Frame::createFrame();
+        pFrame->camera_ = camera;
+        pFrame->color_ = color;
+        pFrame->depth_ = depth;
+        pFrame->time_stamp_ = rgb_times[i];
+
+//        boost::timer timer;
+        vo->addFrame ( pFrame );
+//        cout<<"VO costs time: "<<timer.elapsed() <<endl;
+
+        if ( vo->state_ == gslam::VisualOdometry::LOST )
+            break;
+        Sophus::SE3<double> Twc = pFrame->T_c_w_.inverse();
+
+        // show the map and the camera pose
+        cv::Affine3d M (
+            cv::Affine3d::Mat3 (
+                Twc.rotationMatrix() ( 0,0 ), Twc.rotationMatrix() ( 0,1 ), Twc.rotationMatrix() ( 0,2 ),
+                Twc.rotationMatrix() ( 1,0 ), Twc.rotationMatrix() ( 1,1 ), Twc.rotationMatrix() ( 1,2 ),
+                Twc.rotationMatrix() ( 2,0 ), Twc.rotationMatrix() ( 2,1 ), Twc.rotationMatrix() ( 2,2 )
+            ),
+            cv::Affine3d::Vec3 (
+                Twc.translation() ( 0,0 ), Twc.translation() ( 1,0 ), Twc.translation() ( 2,0 )
+            )
+        );
+
+        Mat img_show = color.clone();
+        for ( auto& pt:vo->map_->map_points_ )
+        {
+            gslam::MapPoint::Ptr p = pt.second;
+            Vector2d pixel = pFrame->camera_->world2pixel ( p->pos_, pFrame->T_c_w_ );
+            cv::circle ( img_show, cv::Point2f ( pixel ( 0,0 ),pixel ( 1,0 ) ), 5, cv::Scalar ( 0,255,0 ), 2 );
+        }
+
+        cv::imshow ( "image", img_show );
+        cv::waitKey ( 1 );
+        vis.setWidgetPose ( "Camera", M );
+        vis.spinOnce ( 1, false );
+
+        cout<<endl;
+    }
+
+    return 0;
+}
